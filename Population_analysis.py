76364485561b037ffbaec8fe84bc5fd7b36b2caf
CMcLachlan <mclachlan-c@ulster.ac.uk>
2023-06-11 16:32:07 +0100

    Clip LSOAs to project area and buffer area
    Clip LSOAs and join "car and van availability" data to clipped areas

--- Population_analysis.py
+++ Population_analysis.py
@@ -11,3 +11,14 @@ Car_van = pd.read_csv(
     r'C:/Users/charl/Documents/Essex Wildlife Trust/Population analysis private/census2021-ts045-lsoa.csv')
 LSOAs = gpd.read_file(r'C:/Users/charl/Documents/Essex Wildlife Trust/Population analysis private/LSOA_2021_EW_BGC.shp')
 
+# CLIP LSOAs TO PROJECT AREA AND BUFFER AREA
+ProjLSOAs = gpd.clip(LSOAs, ProjectArea)
+BuffLSOAs = gpd.clip(LSOAs, ProjectBuffer)
+
+ProjLSOAs.to_file(r'C:/Users/charl/Documents/Essex Wildlife Trust/Population analysis private/ProjLSOAs.shp')
+BuffLSOAs.to_file(r'C:/Users/charl/Documents/Essex Wildlife Trust/Population analysis private/BuffLSOAs.shp')
+
+# FILTER DATA TO APPROPRIATE AREA(S)
+ProjCarVan = pd.merge(Car_van, ProjLSOAs, how='left', left_on=['geography code'], right_on=['LSOA21CD'])
+BuffCarVan = pd.merge(Car_van, BuffLSOAs, how='left', left_on=['geography code'], right_on=['LSOA21CD'])
+
